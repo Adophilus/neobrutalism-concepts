e500505d46a3652eaa5be8ffacb2849d2fe3f5c4
Adophilus <uchenna19of@gmail.com>
2023-10-22 08:16:41 +0100

    updated the repo

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './*.html'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        grotesque: ['Bricolage Grotesque', 'sans-serif'],
+        archivo: ['Archivo', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
